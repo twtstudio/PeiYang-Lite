9eed5511ae1a4dc5bb537686101a50e9c0c155c8
游奕桁 <yuuekiketa@youyihengdeMacBook-Pro.local>
2021-03-12 16:44:26 +0800

    [fix]-自习室详情页随机颜色“

--- PeiYang Lite/StudyRoom/View/StudyRoomContentView.swift
+++ PeiYang Lite/StudyRoom/View/StudyRoomContentView.swift
@@ -11,6 +11,7 @@ struct StudyRoomContentView: View {
     let courseArray: [Course]
     let status: [String]
     let width: CGFloat
+    
     private var colorHelper: ColorHelper { ColorHelper.shared }
     
     var body: some View {
@@ -30,8 +31,8 @@ struct StudyRoomContentView: View {
             // MARK: - Course
             ForEach(0...6, id: \.self) { weekday in
                 ZStack(alignment: .top) {
-                    ForEach(0...5, id:\.self) { i in
-                        if status[weekday][2*i] == "1" {
+                    ForEach(1...6, id:\.self) { i in
+                        if status[weekday][2*i-1] == "1" {
                             StudyRoomRoomView(index: i, width: width)
                         }
                     }
@@ -59,6 +60,10 @@ struct StudyRoomContentView_Previews: PreviewProvider {
 struct StudyRoomRoomView: View {
     var index: Int
     let width: CGFloat
+    let colorArray = [
+        #colorLiteral(red: 0.5590268373, green: 0.5717645288, blue: 0.645496428, alpha: 1), #colorLiteral(red: 0.4453202486, green: 0.4580122828, blue: 0.5316858888, alpha: 1), #colorLiteral(red: 0.5585952401, green: 0.4775523543, blue: 0.5879413486, alpha: 1), #colorLiteral(red: 0.512439549, green: 0.524015069, blue: 0.6323540807, alpha: 1), #colorLiteral(red: 0.3847824335, green: 0.396281302, blue: 0.4800215364, alpha: 1), #colorLiteral(red: 0.6332313418, green: 0.6521518826, blue: 0.7899104953, alpha: 1), #colorLiteral(red: 0.6195089221, green: 0.5867381692, blue: 0.7159363031, alpha: 1), #colorLiteral(red: 0.6934235692, green: 0.6567432284, blue: 0.8013477921, alpha: 1),
+        #colorLiteral(red: 0.4496340156, green: 0.3764404655, blue: 0.4869975448, alpha: 1), #colorLiteral(red: 0.6253550649, green: 0.5235865116, blue: 0.6773356795, alpha: 1), #colorLiteral(red: 0.7457726598, green: 0.6244233251, blue: 0.8077706695, alpha: 1), #colorLiteral(red: 0.4722867608, green: 0.5927650928, blue: 0.6177451015, alpha: 1), #colorLiteral(red: 0.4739673138, green: 0.6488676667, blue: 0.6929715872, alpha: 1), #colorLiteral(red: 0.2412205935, green: 0.3298183084, blue: 0.3522273302, alpha: 1), #colorLiteral(red: 0.218701601, green: 0.3526560962, blue: 0.2918043733, alpha: 1), #colorLiteral(red: 0.04705882353, green: 0.2784313725, blue: 0.4039215686, alpha: 1)
+    ].shuffled()
     
     var body: some View {
         Text("课程占用")
@@ -70,8 +75,8 @@ struct StudyRoomRoomView: View {
                 width: width*1.1,
                 height: width * 3
             )
-            .background(Color(#colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)))
+            .background(Color(colorArray[index]))
             .clipShape(RoundedRectangle(cornerRadius: 8))
-            .offset(y: 2 * CGFloat(index) * width * 1.52)
+            .offset(y: 2 * CGFloat(index-1) * width * 1.52)
     }
 }
