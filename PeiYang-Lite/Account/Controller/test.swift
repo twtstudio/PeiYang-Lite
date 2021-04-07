0ecf7a332a0b6c019e9b17feaa98f5d6a7c408d1
Zrzzzz <1160026659@qq.com>
2021-04-07 13:15:30 +0800

    [Fix] sync

---
+++ PeiYang-Lite/Account/Controller/test.swift
@@ -0,0 +1,24 @@
+//
+//  test.swift
+//  PeiYang-Lite
+//
+//  Created by Zr埋 on 2021/4/7.
+//
+
+import SwiftUI
+
+struct test: View {
+    var body: some View {
+        ZStack {
+            VStack {
+                Color.red
+            }
+        }
+    }
+}
+
+struct test_Previews: PreviewProvider {
+    static var previews: some View {
+        test()
+    }
+}
